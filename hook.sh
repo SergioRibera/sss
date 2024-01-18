42dc8dbe70364f382e215f4bfa1190cc00d533c8
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-01-18 17:10:30 -0400

    feat: add replacement hook of versions

---
+++ hook.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sed -i "s#version = \"${PREV_VERSION}\"#version = \"${NEW_VERSION}\"#g" ../../default.nix
+sed -i "s#${CRATE_NAME}/v${PREV_VERSION}#${CRATE_NAME}/v${NEW_VERSION}#g" ../../default.nix
