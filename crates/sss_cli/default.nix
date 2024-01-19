7d0bf94f2e0b0afe422d516a1d90a3b90ca52acb
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-01-19 16:54:50 -0400

    try: replace musl by gnu

--- crates/sss_cli/default.nix
+++ crates/sss_cli/default.nix
@@ -2,7 +2,7 @@
 let
   pkgs = import <nixpkgs> { };
   platform = {
-    "x86_64-linux" = "x86_64-unknown-linux-musl";
+    "x86_64-linux" = "x86_64-unknown-linux-gnu";
     "x86_64-darwin" = "x86_64-apple-darwin";
     "aarch64-darwin" = "aarch64-apple-darwin";
   }."${pkgs.stdenv.hostPlatform.system}";
