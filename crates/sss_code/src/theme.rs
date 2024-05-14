8d3f8c5c00ec20f03b0e0489c2b7f28831e53622
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-14 17:28:09 -0400

    chore(code): remove unwrap from theme loader

--- crates/sss_code/src/theme.rs
+++ crates/sss_code/src/theme.rs
@@ -9,27 +9,28 @@ mod vim;
 
 pub use vim::theme_from_vim;
 
+use crate::error::CodeScreenshot;
+
 pub fn list_themes(ts: &ThemeSet) {
     for t in ts.themes.keys() {
         println!("- {}", t);
     }
 }
 
-pub fn load_theme(tm_file: &str, enable_caching: bool) -> Theme {
+pub fn load_theme(tm_file: &str, enable_caching: bool) -> Result<Theme, CodeScreenshot> {
     let tm_path = Path::new(tm_file);
 
     if enable_caching {
         let tm_cache = tm_path.with_extension("tmdump");
 
         if tm_cache.exists() {
-            from_dump_file(tm_cache).unwrap()
+            Ok(from_dump_file(tm_cache).unwrap())
         } else {
-            let theme =
-                ThemeSet::get_theme(tm_path).expect(&format!("Theme {tm_path:?} not found"));
+            let theme = ThemeSet::get_theme(tm_path)?;
             dump_to_file(&theme, tm_cache).unwrap();
-            theme
+            Ok(theme)
         }
     } else {
-        ThemeSet::get_theme(tm_path).expect(&format!("Theme {tm_path:?} not found"))
+        Ok(ThemeSet::get_theme(tm_path)?)
     }
 }
