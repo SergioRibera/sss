68c955240c1bb563d43437afb40727cccdee1fa4
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-13 22:52:23 -0400

    chore: add pretty error wrapper

--- crates/sss_lib/src/error.rs
+++ crates/sss_lib/src/error.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fmt::{self, Write};
 use font_kit::error::{FontLoadingError, SelectionError};
 use std::num::ParseIntError;
 
@@ -36,4 +38,106 @@ pub enum ParseColor {
 pub enum FontError {
     SelectionError(#[from] SelectionError),
     FontLoadingError(#[from] FontLoadingError),
+}
+
+// this code is inspiration from https://github.com/Kijewski/pretty-error-debug/blob/main/src/implementation.rs
+/// Wrap an [`Error`] to display its error chain in debug messages ([`format!("{:?}")`][fmt::Debug]).
+///
+/// ```
+#[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct PrettyErrorWrapper<E: 'static + Error>(pub E);
+
+impl<E: 'static + Error> PrettyErrorWrapper<E> {
+    /// Return the wrapped argument.
+    #[inline]
+    pub fn new(err: E) -> Self {
+        Self(err)
+    }
+}
+
+impl<E: 'static + Error> From<E> for PrettyErrorWrapper<E> {
+    #[inline]
+    fn from(value: E) -> Self {
+        Self(value)
+    }
+}
+
+impl<E: 'static + Error> Error for PrettyErrorWrapper<E> {
+    #[inline]
+    fn source(&self) -> Option<&(dyn 'static + Error)> {
+        Some(&self.0)
+    }
+}
+
+impl<E: 'static + Error> fmt::Display for PrettyErrorWrapper<E> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl<E: 'static + Error> fmt::Debug for PrettyErrorWrapper<E> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let error = &self.0;
+        write!(f, "{}", error)?;
+        if let Some(cause) = error.source() {
+            write!(f, "\n\nCaused by:")?;
+            let multiple = cause.source().is_some();
+            for (n, error) in Chain(Some(cause)).enumerate() {
+                writeln!(f)?;
+                let mut indented = Indented {
+                    inner: f,
+                    number: if multiple { Some(n + 1) } else { None },
+                    started: false,
+                };
+                write!(indented, "{}", error)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+struct Chain<'a>(Option<&'a dyn Error>);
+
+impl<'a> Iterator for Chain<'a> {
+    type Item = &'a dyn Error;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        let error = self.0?;
+        self.0 = error.source();
+        Some(error)
+    }
+}
+
+struct Indented<'a, 'b: 'a> {
+    inner: &'a mut fmt::Formatter<'b>,
+    number: Option<usize>,
+    started: bool,
+}
+
+impl<'a, 'b: 'a> fmt::Write for Indented<'a, 'b> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for (i, line) in s.split('\n').enumerate() {
+            if !self.started {
+                self.started = true;
+                match self.number {
+                    Some(number) => write!(self.inner, "{: >5}: ", number)?,
+                    None => self.inner.write_str("    ")?,
+                }
+            } else if i > 0 {
+                self.inner.write_char('\n')?;
+                if self.number.is_some() {
+                    self.inner.write_str("       ")?;
+                } else {
+                    self.inner.write_str("    ")?;
+                }
+            }
+
+            self.inner.write_str(line)?;
+        }
+
+        Ok(())
+    }
 }
