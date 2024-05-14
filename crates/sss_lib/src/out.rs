c18c1e8f49d3c869b7416c375234130fd84a965f
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-05-14 03:29:27 -0400

    chore: remove unwraps from out

--- crates/sss_lib/src/out.rs
+++ crates/sss_lib/src/out.rs
@@ -5,44 +5,46 @@ use notify_rust::Notification;
 #[cfg(target_os = "linux")]
 use notify_rust::Image;
 
-use crate::str_to_format;
+use crate::{error, str_to_format};
 
 pub fn make_output(
     img: &ImageBuffer<Rgba<u8>, Vec<u8>>,
     output: &str,
     show_notify: bool,
     fmt: Option<&str>,
-) {
+) -> Result<(), error::ImagenGeneration> {
+    log::trace!("Making Output");
     match output {
         "raw" => {
             let mut stdout = std::io::stdout();
             let encoder = PngEncoder::new(&mut stdout);
-            encoder
-                .write_image(img, img.width(), img.height(), image::ColorType::Rgba8)
-                .unwrap();
+            encoder.write_image(img, img.width(), img.height(), image::ColorType::Rgba8)?;
         }
         _ => {
-            img.save_with_format(&output, str_to_format(fmt.unwrap_or("png")).unwrap())
-                .unwrap();
+            let format_img = str_to_format(fmt.unwrap_or("png"))?;
+            log::debug!("Format Image to save: {format_img:?}");
+            img.save_with_format(&output, format_img)?;
 
             if show_notify {
+                log::trace!("Show notification");
                 #[cfg(all(unix, not(target_os = "macos"), not(target_os = "windows")))]
                 Notification::new()
                     .summary("Image generated")
                     .body(&format!("Image stored in {output}"))
-                    .image_data(
-                        Image::from_rgba(img.width() as i32, img.height() as i32, img.to_vec())
-                            .unwrap(),
-                    )
-                    .show()
-                    .unwrap();
+                    .image_data(Image::from_rgba(
+                        img.width() as i32,
+                        img.height() as i32,
+                        img.to_vec(),
+                    )?)
+                    .show()?;
                 #[cfg(all(target_os = "macos", target_os = "windows"))]
                 Notification::new()
                     .summary("Image generated")
                     .body(&format!("Image stored in {output}"))
-                    .show()
-                    .unwrap();
+                    .show()?;
             }
         }
     }
+    log::trace!("End Output");
+    Ok(())
 }
