f50dec9d33d999af9967c32df5b108a578e14da7
Nat <rodrigezsantiago40@gmail.com>
2024-05-23 03:30:11 +0000

    Add catppuccin theme to the sync script

--- assets/sync_from_bat.sh
+++ assets/sync_from_bat.sh
@@ -4,11 +4,17 @@ if [[ -d ../../bat ]]; then
   git -C ../../bat pull
 else
   git clone --depth 1 --recurse-submodules https://github.com/sharkdp/bat ../../bat
+  if [[ -d ../../bat_catppuccin ]]; then
+    git -C ../../bat_catppuccin pull
+  else
+    git clone --depth 1 --recurse-submodules https://github.com/catppuccin/bat ../../bat_catppuccin
+  fi
 fi
 
 rm -rf ./syntaxes/* ./themes/*
 cp -r ../../bat/assets/syntaxes/* ./syntaxes/
 cp -r ../../bat/assets/themes/* ./themes/
+cp -r ../../bat_catppuccin/themes/* ./themes
 
 cargo run -p sss_code -- --build-cache . -o .
 
