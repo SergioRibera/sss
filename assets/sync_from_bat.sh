1eae4206a7372519cddcfd173e2d9e96da78764e
NatProgramer <rodrigezsantiago40@gmail.com>
2024-05-22 22:57:00 -0500

    Change in script to use bat_themes repositority

--- assets/sync_from_bat.sh
+++ assets/sync_from_bat.sh
@@ -4,17 +4,17 @@ if [[ -d ../../bat ]]; then
   git -C ../../bat pull
 else
   git clone --depth 1 --recurse-submodules https://github.com/sharkdp/bat ../../bat
-  if [[ -d ../../bat_catppuccin ]]; then
-    git -C ../../bat_catppuccin pull
+  if [[ -d ../../bat_themes ]]; then
+    git -C ../../bat_themes pull
   else
-    git clone --depth 1 --recurse-submodules https://github.com/catppuccin/bat ../../bat_catppuccin
+    git clone --depth 1 --recurse-submodules https://github.com/NatProgramer/bat_themes ../../bat_themes
   fi
 fi
 
 rm -rf ./syntaxes/* ./themes/*
 cp -r ../../bat/assets/syntaxes/* ./syntaxes/
 cp -r ../../bat/assets/themes/* ./themes/
-cp -r ../../bat_catppuccin/themes/* ./themes
+cp -r ../../bat_themes/* ./themes
 
 cargo run -p sss_code -- --build-cache . -o .
 
